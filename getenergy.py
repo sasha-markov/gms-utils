b7fbf09d9f9178bc1b0def580cb4126f5db83beb
sasha-markov <hello@sasha-markov.net>
2015-06-17 15:11:48 +0300

    added get_thermochem and get_mo_energies functions

--- getenergy.py
+++ getenergy.py
@@ -28,3 +28,72 @@ def getzpe(logfile):
         if not sw:
             print "There is no ZPE data in the file!"
     return eval(s[0])
+
+
+def get_thermochem(logfile):
+    with open(logfile, 'r') as f:
+        pat = re.compile(r"""
+                          TOTAL
+                         \s+\d+.\d+
+                         \s+\d+.\d+
+                         \s+\D\d+.\d+
+                         \s+\d+.\d+
+                         \s+\d+.\d+
+                         \s+\d+.\d+
+                         """, re.VERBOSE)
+        ss = re.findall(pat, f.read())
+        s = ss[-1].split()
+        H, G, S = s[2], s[3], s[-1]
+    return eval(H), eval(G), eval(S)
+
+
+def get_mo_energies(logfile):
+    with open(logfile, 'r') as f:
+        swa = False
+        swb = False
+        swc = False
+        swd = False
+        pat = re.compile(r"""
+                        \s*
+                        -?\d+\.\d+
+                    """, re.VERBOSE)
+        eigveca = []
+        eigvecb = []
+        mosalpha = []
+        mosbeta = []
+        for line in f:
+            if (('          EIGENVECTORS') in line) and not swb:
+                swa = True
+                continue
+            if swa:
+                if pat.match(line):
+                    for i in line.split():
+                        eigveca.append(eval(i))
+                if re.match(r'  ----- BETA SET ----- ', line):
+                    swa = False
+                    swb = True
+            if swb:
+                if pat.match(line):
+                    for i in line.split():
+                        eigvecb.append(eval(i))
+            if re.match(r' ...... END OF (U|R)HF CALCULATION ......', line):
+                swa = False
+                swb = False
+            if ('          MOLECULAR ORBITALS') in line:
+                swc = True
+                continue
+            if swc:
+                if pat.match(line):
+                    for i in line.split():
+                        mosalpha.append(eval(i))
+                if ('          **** BETA SET ****') in line:
+                    swc = False
+                    swd = True
+            if swd:
+                if pat.match(line):
+                    for i in line.split():
+                        mosbeta.append(eval(i))
+            if ('     PROPERTIES FOR THE') in line:
+                swc = False
+                swd = False
+    return eigveca, eigvecb, mosalpha, mosbeta
